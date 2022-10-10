bbaf005e1394ba04e61c21ee44d98a764ee1f6c3
Angel Vasilev <angavasilev@gmail.com>
2022-10-10 22:52:47 +0300

    finish offer entity

--- mobilelele/src/main/java/bg/softuni/mobilelele/model/entity/OfferEntity.java
+++ mobilelele/src/main/java/bg/softuni/mobilelele/model/entity/OfferEntity.java
@@ -1,6 +1,7 @@
 package bg.softuni.mobilelele.model.entity;
 
 import bg.softuni.mobilelele.model.entity.enums.EngineEnum;
+import bg.softuni.mobilelele.model.entity.enums.TransmissionEnum;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
@@ -28,15 +29,109 @@ public class OfferEntity {
 
 	private int mileage;
 
+	@Column(nullable = false)
 	private BigDecimal price;
 
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TransmissionEnum transmission;
+
+	private int year;
+
+	@ManyToOne
+	private ModelEntity model;
+
+	@ManyToOne
+	private UserEntity seller;
 
 	public UUID getId() {
 		return id;
 	}
 
-	public OfferEntity setId(UUID id) {
-		this.id = id;
+	public EngineEnum getEngine() {
+		return engine;
+	}
+
+	public OfferEntity setEngine(EngineEnum engine) {
+		this.engine = engine;
 		return this;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public OfferEntity setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+		return this;
+	}
+
+	public int getMileage() {
+		return mileage;
+	}
+
+	public OfferEntity setMileage(int mileage) {
+		this.mileage = mileage;
+		return this;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public OfferEntity setPrice(BigDecimal price) {
+		this.price = price;
+		return this;
+	}
+
+	public TransmissionEnum getTransmission() {
+		return transmission;
+	}
+
+	public OfferEntity setTransmission(TransmissionEnum transmission) {
+		this.transmission = transmission;
+		return this;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public OfferEntity setYear(int year) {
+		this.year = year;
+		return this;
+	}
+
+	public ModelEntity getModel() {
+		return model;
+	}
+
+	public OfferEntity setModel(ModelEntity model) {
+		this.model = model;
+		return this;
+	}
+
+	public UserEntity getSeller() {
+		return seller;
+	}
+
+	public OfferEntity setSeller(UserEntity seller) {
+		this.seller = seller;
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return "OfferEntity{" +
+				"id=" + id +
+				", engine=" + engine +
+				", imageUrl='" + imageUrl + '\'' +
+				", mileage=" + mileage +
+				", price=" + price +
+				", transmission=" + transmission +
+				", year=" + year +
+				", model=" + model +
+				", seller=" + seller +
+				'}';
 	}
 }
